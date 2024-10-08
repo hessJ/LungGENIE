772673a8126a7a9b5b860971461dce7725b53f23
Jonathan Hess <hessjo@upstate.edu>
2024-10-08 12:24:16 -0400

    source code

--- lunggenie_methods.R
+++ lunggenie_methods.R
@@ -125,8 +125,7 @@ predict_pca = function(dat = NULL, pca_model = NULL, mean_imputation = FALSE){
 
 
 # 5. Fit LR-PCA prediction model to GTEx counts, then apply the resulting weights to the PCA scores derived from new samples
-fit_lr_weights_in_gtex = function(pca_model  = NULL, tissue = NULL, gene_list = NULL, n_comps = 20){
-  if(is.null(tissue)){stop("Please specify a tissue based on nomenclature available in the cross-validation table")}
+fit_lr_weights_in_gtex = function(pca_model  = NULL,  gene_list = NULL, n_comps = 20){
   if(is.null(pca_model)){stop("Argument required for blood_PCA")}
   if(is.null(gene_list)){stop("Please specify gene IDs to include in the training process.")}
   # TODO: Linear regression: lung transcript ~ blood PCA
@@ -144,27 +143,7 @@ fit_lr_weights_in_gtex = function(pca_model  = NULL, tissue = NULL, gene_list = 
   fit = lm(as.matrix(Y) ~ ., data = X) # fit a LR model per gene
   return(fit) # return model
 }
-
-fit_lr_weights_in_gtex_age_sex = function(pca_model  = NULL, tissue = NULL, gene_list = NULL, n_comps = 20){
-  if(is.null(tissue)){stop("Please specify a tissue based on nomenclature available in the cross-validation table")}
-  if(is.null(pca_model)){stop("Argument required for blood_PCA")}
-  if(is.null(gene_list)){stop("Please specify gene IDs to include in the training process.")}
-  # TODO: Linear regression: lung transcript ~ blood PCA
-  Y = data.frame(t(lung_expr)) # select normalized GTEx lung counts
-  # filter Y by genes that are well predicted from 5-fold cross-validation
-  cv_performance = gene_list
-  matched_genes = intersect(colnames(Y), cv_performance)
-  if(length(matched_genes) < 1){stop("Unexpected issue! No matching gene IDs between CV performance file and normalized GTEx counts.")}
-  common_genes = intersect(matched_genes, pca_model$genes)
-  if(length(matched_genes) < 1){stop("Unexpected issue! No genes available to run LR.")}
-  message("\rTraining LR models for: ", length(matched_genes), " genes")
-  
-  Y = Y[,colnames(Y) %in% matched_genes, ]
-  X = data.frame(pca_model$pca$x[,1:n_comps]) # use PCA model derived from GTEx paired blood-lung data
-  X = data.frame(load_predictors[,!colnames(load_predictors) %in% "SampleID"], X) # bind age, sex, and blood PCs
-  fit = lm(as.matrix(Y) ~ ., data = X) # fit a LR model per gene
-  return(fit) # return model
-}
+ 
 
 # 6. apply lung transcriptome prediction model to blood PCA
 impute_gxp = function(pca_in_new_sample = NULL, trained_model = NULL, scale = TRUE){
@@ -186,23 +165,7 @@ impute_gxp = function(pca_in_new_sample = NULL, trained_model = NULL, scale = TR
   
   imputed_ci <<- predict_ci
 }
-
-impute_gxp_with_age_sex = function(pca_in_new_sample = NULL, trained_model = NULL, scale = TRUE){
-  
-  if(is.null(trained_model)){stop("Please specify object containing pre-trained models")}
-  
-  if(is.null(pca_in_new_sample)){stop("Please provide blood-based transcriptome PCs for new samples")}
-  
-  predict_vals = predict(trained_model, data.frame(pca_in_new_sample))
-  
-  if(scale == TRUE){
-    
-    predict_vals = scale(predict_vals)
-  }
-  
-  return(data.frame(predict_vals, check.names = F))
-}
-
+ 
 
 # additional functions:
 
@@ -253,7 +216,7 @@ retrain_gtex = function(gene_list = NULL, output = "", set_seed = T, seed = 123,
   if(output == ""){warning("No output directory specified!")}
   
   # make output file path into string for loading CV performance
-  output_file_string <<- paste(output, "/", tissue, "_LungGENIE_retrain-", ncomps, ".Rdata", sep="")
+  output_file_string <<- paste(output, "/LungGENIE_retrain-", ncomps, ".Rdata", sep="")
   
   use_n_comps = ncomps
   
@@ -426,10 +389,9 @@ retrain_gtex = function(gene_list = NULL, output = "", set_seed = T, seed = 123,
     all.models.merge = perf_train
   }
   
-  all.models.merge$tissue = tissue
   
   # save output as .Rdata file
-  saveRDS(all.models.merge, file=paste(output, "/", tissue, "_LungGENIE_retrain-",ncomps,".Rdata", sep=""))
+  saveRDS(all.models.merge, file=paste(output, "/LungGENIE_retrain-",ncomps,".Rdata", sep=""))
   
   perf <<- all.models.merge
   
